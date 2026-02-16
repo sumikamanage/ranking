4e1e6ba5579b2ec0fa525ddce21bbbe95c5d657d
sumikamanage <sumika.manage@gmail.com>
2026-02-16 12:08:02 +0900

    Create main.py

---
+++ sumika-ranking/main.py
@@ -0,0 +1,57 @@
+import os
+import json
+import asyncio
+import time
+from datetime import datetime
+from threading import Thread
+import config
+
+import discord
+from discord.ext import commands
+
+from flask import Flask, redirect, request, session, url_for, jsonify
+from waitress import serve
+
+# 自作モジュール
+#botインスタンス読み込み
+from bot_core.bot import bot
+
+#flask起動
+from web_app.app import app
+
+
+# bot落ち防止のためのuptimerobot用,起動に必要
+port = int(os.environ.get("PORT", 8080))
+
+def run():
+    try:
+        print(f"✅ Flaskサーバーを {port} ポートで起動")
+        serve(app, host="0.0.0.0", port=port)
+    except Exception as e:
+        print("❌ Flaskサーバーエラー:", e)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+print("🔄 サーバーを定義完了")
+
+# 実行部
+
+async def main():
+    async with bot:
+        await bot.start(TOKEN)
+
+        
+
+
+
+# --- 実行セクション ---
+
+
+
+if __name__ == "__main__":
+    asyncio.sleep(30)
+    keep_alive()
+    asyncio.run(main())
