f142e27d6455c578a450f5a298cd812d4a7d1c34
sumika-ranking <sumika.ranking@gmail.com>
2026-02-26 15:52:32 +0900

    Update config.py

--- sumika-ranking/config.py
+++ sumika-ranking/config.py
@@ -2,8 +2,7 @@
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "messages.db")
+
 
 TOKEN = os.getenv("TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
