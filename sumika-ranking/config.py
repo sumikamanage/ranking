90b062c9d1d2011b321d4ce238d3b0606ffde398
sumika-ranking <sumika.ranking@gmail.com>
2026-02-26 16:32:15 +0900

    Update config.py

--- sumika-ranking/config.py
+++ sumika-ranking/config.py
@@ -4,6 +4,10 @@ import os
 
 
 
+# --- DB_PATH をこのファイルと同じディレクトリに固定 ---
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+DB_PATH = os.path.join(BASE_DIR, "messages.db")
+
 TOKEN = os.getenv("TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
 APP_ID = int(os.getenv("APPLICATION_ID"))
